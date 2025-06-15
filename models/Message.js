e0133ac45d96e497fb1a7fbf346531ad142c3b94
sailendrakondapalli <127012315+sailendrakondapalli@users.noreply.github.com>
2025-06-15 10:56:03 +0530

    Initial commit

--- models/Message.js
+++ models/Message.js
@@ -1,3 +1,13 @@
+// const mongoose = require('mongoose');
+// const MessageSchema = new mongoose.Schema({
+//   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+//   message: String,
+//   timestamp: { type: Date, default: Date.now },
+//   read: { type: Boolean, default: false }
+// });
+// module.exports = mongoose.model('Message', MessageSchema);
+
 const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
--- models/User.js
+++ models/User.js
@@ -1,3 +1,12 @@
+// const mongoose = require('mongoose');
+// const UserSchema = new mongoose.Schema({
+//   username: { type: String, required: true, unique: true },
+//   password: { type: String, required: true },
+//   gender: String,
+//   state: String
+// });
+// module.exports = mongoose.model('User', UserSchema);
+
 const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
